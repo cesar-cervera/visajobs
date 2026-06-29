c075a6bb7be50147b97241d07c6a4a197c7cc614
Farah Aburokba <farahaburokba@Farahs-MacBook-Air.local>
2026-06-29 11:53:50 -0400

    Environmens file added

---
+++ public/src/environments.js
@@ -0,0 +1,3 @@
+export const APPLICATION_ID ='uwtwp4zQlAy4qJukuaCuaLg6nteuInCCJtGRcD7Y';
+export const JAVASXRIPT_KET ='sxgCdc2thT6B8GVsswL8wYRhy5pwoKIynxcql0xy';
+export const SERVER_URL ='https://parseapi.back4app.com';
