9fb3529be51cfb14a84ef06286ce242d7a759c14
aburokbafarah <faburokb@nd.edu>
2026-06-29 12:46:58 -0400

    Fix merge conflict in environments.js
    Removed merge conflict markers and cleaned up environment variables.

--- public/src/environments.js
+++ public/src/environments.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 export const APPLICATION_ID ='uwtwp4zQlAy4qJukuaCuaLg6nteuInCCJtGRcD7Y';
 export const JAVASCRIPT_KEY ='sxgCdc2thT6B8GVsswL8wYRhy5pwoKIynxcql0xy';
 export const SERVER_URL ='https://parseapi.back4app.com';
-=======
 
->>>>>>> 804293f4538946e2cd6002d9b0410d388cdbfe3c
